9060ffa3083b65c9335a03683bdb08408a1ddaf5
peter-mwovi <petermmwovi@gmail.com>
2023-09-23 20:42:45 +0300

    Edit Categorylist.js file to resolve linters issue

--- src/components/Categorylist.js
+++ src/components/Categorylist.js
@@ -1,5 +1,5 @@
 // import React, { useState } from 'react';
-import Button from './Button';
+import Button from './Button.js';
 
 function Categorylist() {
   // const [category, displayCategory] = useState({
