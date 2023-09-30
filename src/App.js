36239ab977aee7b71ab96598a715a19dfd2280a1
peter-mwovi <petermmwovi@gmail.com>
2023-09-30 19:04:08 +0300

    Edit src files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
-import store from './redux/store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="categories" element={<Categories />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
