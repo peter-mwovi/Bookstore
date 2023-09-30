5693c51950529f62c150af1d8e12549d95f67982
peter-mwovi <petermmwovi@gmail.com>
2023-09-30 19:04:27 +0300

    Add boosSlice.js file

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,12 +2,32 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState: [
+    {
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
   reducers: {
     addBook: (state, action) => {
       const newId = state.length === 0 ? 1 : state[state.length - 1].id + 1;
+      const idText = `item${newId}`;
       const newBook = {
-        id: newId,
+        id: idText,
         title: action.payload.title,
         author: action.payload.author,
       };
